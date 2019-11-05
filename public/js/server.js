a69c59b4e7db69b7d5320ba34fb5b914bff3e887
Jake Carlson <jakecarlson@Jakes-MBP.zyxel.com>
2019-11-05 01:24:21 -0600

    added carousel

--- public/js/server.js
+++ public/js/server.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
 console.log("Hello");
 $('.sidenav').sidenav();
+console.log("sidenav");
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true
+  });
+  console.log("carousel");
+  $('.parallax').parallax();
+console.log("parallax");
 });
